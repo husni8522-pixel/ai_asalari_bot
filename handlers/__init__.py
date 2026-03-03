7f98a173ce5e7c10d6b59743d75791cbfb205230
husni8522-pixel <husni8522@gmail.com>
2026-03-03 12:36:58 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,14 +2,18 @@ from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, f
 
 from .start_handler import start
 from .text_handler import text_handler
-from .callback_handler import reset_cb, lang_cb
+from .callback_handler import reset_cb, lang_cb, level_cb
 from .admin_panel import admin_conv
 
 
 def register_handlers(app):
     app.add_handler(admin_conv)
+
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
+
     app.add_handler(CallbackQueryHandler(reset_cb, pattern="^reset$"))
     app.add_handler(CallbackQueryHandler(lang_cb, pattern="^lang_"))
-    
+
+    # 🔥 SHU YO‘Q EDI
+    app.add_handler(CallbackQueryHandler(level_cb, pattern="^level_"))
